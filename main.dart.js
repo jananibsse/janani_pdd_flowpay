9c14b1604c57e6133996a581517e6575254a9f07
jananibsse <jananibsse@users.noreply.github.com>
2026-07-21 04:54:16 +0000

    deploy: b94aef2d123a1246c59935a4896205c38fb3e79c

--- main.dart.js
+++ main.dart.js
@@ -33443,7 +33443,7 @@ $S:2}
 A.aMP.prototype={
 $1(a){var s=A.dY().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/83675ed27633283e7fc296c8bca22e841224c096/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/69c8c61792f04cc809dfef0c910414fb9afc06cd/":s)+a},
 $S:85}
 A.Qh.prototype={
 a20(){var s=this.J7(),r=$.b9.bG().ImageFilter.MakeColorFilter(s,null)
